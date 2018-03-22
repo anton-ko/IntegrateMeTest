9d579b21c439ebd87cbe5884cc87861607138711
Anton Kolmakov <anton@kolmakov.in>
2018-03-22 16:40:38 -0700

    Style competitions page + add placeholders for better UI

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @competitions = Competition.all.order('created_at desc')
+    @competitions = Competition.all.with_entries_counts.order('competitions.created_at desc')
+    @account = current_account
   end
-
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -4,6 +4,11 @@ class Competition < ActiveRecord::Base
 
   validates_presence_of :name
 
+  scope :with_entries_counts, -> {
+    # consider adding entries_count field and using counter_cache instead
+    select('competitions.*, COUNT(entries.id) as entries_count').joins(:entries).group('competitions.id')
+  }
+
   def subscribes_to_mailing_list?
     account.mailchimp_key.present? && mailing_list_id.present?
   end
